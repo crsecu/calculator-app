3a11707ed58f04badf152b4e590dfa5b9b4162a7
Cristina Secu <secu.cristina@yahoo.com>
2023-10-24 17:24:43 -0400

    style toggle theme radio boxes to look like a toggle switch

--- src/components/Title.js
+++ src/components/Title.js
@@ -8,17 +8,25 @@ export default function Title({updateSelectedTheme, toggleTheme}) {
     updateSelectedTheme(event.target.id);
   };
 
-  let title = ""
+  let title = "";
+  let themeText = "";
+  
 
   switch(toggleTheme) {
     case 'two':
-    title = "t2-btnText"
+    title = "t2-btnText";
+    themeText = "calculator__toggle-theme2"
+    
     break;
     case 'three':
-    title = "t3-btnText"
+    title = "t3-btnText";
+    themeText = "calculator__toggle-theme3"
+    
     break;
     default:
-    title = "whiteText"
+    title = "whiteText";
+    themeText = "calculator__toggle-theme1";
+    
 
   }
 
@@ -27,28 +35,34 @@ export default function Title({updateSelectedTheme, toggleTheme}) {
       <div className="calculator__title-container">
         <h1 className = {title}>calc</h1>
       </div>
-      <div className="calculator__toggle-theme">
-        <input
-          className="calculator__toggle-switch"
-          type="radio"
-          name="toggle"
-          id="one"
-          onChange={handleRadioChange}
-        />
-        <input
-          className="calculator__toggle-switch"
-          type="radio"
-          name="toggle"
-          id="two"
-          onChange={handleRadioChange}
-        />
-        <input
-          className="calculator__toggle-switch"
-          type="radio"
-          name="toggle"
-          id="three"
-          onChange={handleRadioChange}
-        />
+      <div className="calculator__toggle-theme-container">
+        <span className={`calculator__toggle-theme-title ${themeText}`}>THEME</span>
+        <div className = "calculator__toggle-theme">
+          <input
+            className={`calculator__toggle-switch ${toggleTheme === "one" ? "visible" : "hidden"}`}
+            type="radio"
+            name="toggle"
+            id="one"
+            onChange={handleRadioChange}
+          />
+          <label for="one" className="calculator__toggle-switch-label"></label>
+          <input
+            className={`calculator__toggle-switch ${toggleTheme === "two" ? "visible" : "hidden"}`}
+            type="radio"
+            name="toggle"
+            id="two"
+            onChange={handleRadioChange}
+          />
+          <label for="two" className="calculator__toggle-switch-label"></label>
+          <input
+            className={`calculator__toggle-switch ${toggleTheme === "three" ? "visible" : "hidden"}`}
+            type="radio"
+            name="toggle"
+            id="three"
+            onChange={handleRadioChange}
+          />
+          <label for="three" className="calculator__toggle-switch-label"></label>
+        </div>
       </div>
     </div>
   );
